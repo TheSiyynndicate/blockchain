52081a0d43fc388afdbad107fa9ffecae3e27ae8
TheSiyynndicate <59103691+TheSiyynndicate@users.noreply.github.com>
2021-02-06 22:03:00 +0100

    New commit

---
+++ dev/blockchain.js
@@ -0,0 +1,26 @@
+function Blockchain() {
+  this.chain = [];
+  this.newTransactions = [];
+}
+
+Blockchain.prototype.createNewBlock = function (
+  nonce,
+  previousBlockHash,
+  hash
+) {
+  const newBlock = {
+    index: this.chain.length + 1,
+    timestamp: Date.now(),
+    transactions: this.newTransactions,
+    nonce: nonce,
+    hash: hash,
+    previousBlockHash: previousBlockHash,
+  };
+  this.newTransactions = [];
+  this.chain.push(newBlock);
+
+  return newBlock;
+};
+
+
+module.exports = Blockchain;
---
+++ dev/test.js
@@ -0,0 +1,9 @@
+const Blockchain = require('./blockchain');
+
+const bitcoin = new Blockchain();
+
+bitcoin.createNewBlock(2389,'OINA%&SGFSVYG','90VGHBVVHNGG');
+bitcoin.createNewBlock(111,'OINA%&577GUU','FG0VGHBVVHNGG');
+bitcoin.createNewBlock(23589,'*(INA%&SGFSVYG','BGVGHBVVHNGG');
+
+console.log(bitcoin);
