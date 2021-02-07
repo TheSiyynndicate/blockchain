2ce93b8914815a1640c83ce597101c342bb5005f
TheSiyynndicate <59103691+TheSiyynndicate@users.noreply.github.com>
2021-02-07 21:02:40 +0100

    SHA256

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,7 +1,7 @@
 function Blockchain() {
   this.chain = [];
-  this.newTransactions = [];
-}
+  this.pendingTransactions = [];
+} 
 
 Blockchain.prototype.createNewBlock = function (
   nonce,
@@ -11,16 +11,40 @@ Blockchain.prototype.createNewBlock = function (
   const newBlock = {
     index: this.chain.length + 1,
     timestamp: Date.now(),
-    transactions: this.newTransactions,
+    transactions: this.pendingTransactions,
     nonce: nonce,
     hash: hash,
     previousBlockHash: previousBlockHash,
   };
-  this.newTransactions = [];
-  this.chain.push(newBlock);
+  this.pendingTransactions = [];
+  this.chain.push(newBlock); 
 
   return newBlock;
 };
 
+Blockchain.prototype.getLastBlock =
+  function () {
+    return this.chain[this.chain.length - 1];
+  };
+
+Blockchain.prototype.createNewTransaction = function (
+  amount,
+  sender,
+  recipient
+) {
+  const newTransaction = {
+    amount: amount,
+    sender: sender,
+    recipient: recipient,
+  };
+  this.pendingTransactions.push(newTransaction);
+
+  return this.getLastBlock()['index'] + 1;
+};
+
+Blockchain.prototype.hashBlock = function(blockData){
+    
+    // ... return '47B5I3B28293283B2H3J32'
+}
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -2,8 +2,16 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(2389,'OINA%&SGFSVYG','90VGHBVVHNGG');
-bitcoin.createNewBlock(111,'OINA%&577GUU','FG0VGHBVVHNGG');
-bitcoin.createNewBlock(23589,'*(INA%&SGFSVYG','BGVGHBVVHNGG');
+bitcoin.createNewBlock(892348,'A90SDNF09A0N','OIANS909A0S9NF');
 
-console.log(bitcoin);
+bitcoin.createNewTransaction(100,'ALEX89ANSD759DFTG','JENNUUN0A5648GF');
+
+bitcoin.createNewBlock(123123,'6758NF09A0N','687O909A0S9NF');
+
+bitcoin.createNewTransaction(50,'ALEX89ANSD759DFTG','JENNUUN0A5648GF');
+bitcoin.createNewTransaction(300,'ALEX89ANSD759DFTG','JENNUUN0A5648GF');
+bitcoin.createNewTransaction(2000,'ALEX89ANSD759DFTG','JENNUUN0A5648GF');
+
+bitcoin.createNewBlock(123123,'AI6734R67GDE','Y655IVE5GS2');
+
+console.log(bitcoin.chain[2]); 
