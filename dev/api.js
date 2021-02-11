1bcdb207ace522e4c1c053e96d2207e9560bc345
TheSiyynndicate <59103691+TheSiyynndicate@users.noreply.github.com>
2021-02-11 22:03:35 +0100

    API completion

--- dev/api.js
+++ dev/api.js
@@ -3,6 +3,11 @@ const app = express();
 const bodyParser = require("body-parser");
 const Blockchain = require("./blockchain");
 
+const { v4: uuidv4 } = require('uuid');
+const uuid = uuidv4();
+
+const nodeAddress = uuid.split("-").join("");
+
 const coin = new Blockchain();
 
 app.use(bodyParser.json());
@@ -10,14 +15,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get("/blockchain", function (req, res) {
   res.send(coin);
-}); 
-
-app.post("/transaction", function (req, res) {
-  console.log(req.body);
-  res.send(`The amount of the transaction is ${req.body.amount} coin(s).`);
 });
 
-app.get("/mine", function (req, res) {});
+app.post("/transaction", function (req, res) {
+  const blockIndex = coin.createNewTransaction(
+    req.body.amount,
+    req.body.sender,
+    req.body.recipient
+  );
+
+  res.json({ note: `Transaction will be added in block ${blockIndex}` });
+});
+
+app.get("/mine", function (req, res) {
+  const lastBlock = coin.getLastBlock();
+  const previousBlockHash = lastBlock["hash"];
+
+  const currentBlockData = {
+    transactions: coin.pendingTransactions,
+    index: lastBlock["index"] + 1,
+  };
+
+  const nonce = coin.proofOfWork(previousBlockHash, currentBlockData);
+  const blockHash = coin.hashBlock(previousBlockHash, currentBlockData, nonce);
+
+  coin.createNewTransaction(12.5, "00", nodeAddress);
+
+  const newBlock = coin.createNewBlock(nonce, previousBlockHash, blockHash);
+
+  res.json({ note: "New block mined successfully", block: newBlock });
+});
 
 app.listen(3000, function () {
   console.log("Listening on port 3000...");
--- dev/blockchain.js
+++ dev/blockchain.js
@@ -65,7 +65,7 @@ Blockchain.prototype.proofOfWork = function (
   while (hash.substring(0, 4) != "0000") {
     nonce++;
     hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
-    console.log(hash);
+    //console.log(hash);
   }
 
   return nonce;
